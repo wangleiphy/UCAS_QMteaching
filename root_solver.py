45ef6c12a7c2329aed111759438e6f0b81a00acb
Lei Wang <wangleiphy@gmail.com>
2017-09-12 17:56:03 +0800

    minor

--- root_solver.py
+++ root_solver.py
@@ -1,14 +1,14 @@
 from scipy.optimize import bisect
-from math import tan , sqrt, pi
+from math import tan, sqrt, pi
 
 r = 2.0 
 
 def func(x, n):
-    return x * tan(x + n*pi/2) - sqrt(r**2 - x**2)
+    return x * tan(x - n*pi/2) - sqrt(r**2 - x**2)
 
 for n in range(2):
     alpha = bisect(lambda x: func(x, n), n*pi/2, min((n+1)*pi/2, r))
     beta = sqrt(r**2 - alpha**2)
 
     energy = -2*beta**2
-    print n, alpha, beta, energy
+    print 'alpha, beta, energy: ', alpha, beta, energy
